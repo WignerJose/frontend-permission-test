fc2f77567230098f612ae69fef1b2723764455cd
Jose Wigner <jwlliully@fassil.com.bo>
2022-05-31 23:12:29 -0400

    fix page create permission

--- src/pages/CreatePermission/create_permission.page.tsx
+++ src/pages/CreatePermission/create_permission.page.tsx
@@ -2,18 +2,19 @@ import { useState } from "react";
 import { Button, FloatingLabel, Form } from "react-bootstrap";
 import { useAsync, useService } from "../../hooks/useAsync.hook";
 import useFetchAndLoad from "../../hooks/useFetchAndLoad";
-import { Permission } from "../../models/permission.model";
 import { PermissionType } from "../../models/permission_type.model";
 import { getPermissionTypes } from "../../services/client_permission.service";
 import './create_permission.css';
 
 const CreatePermissionPage = () => {
- 
+
     const [employyeName, setName] = useState('');
     const [employyeLastName, setLastName] = useState('');
     const [permissionTypeSelect, setPermissionType] = useState(0);
-    const [loading, setLoading] = useState(false);
+    const [initLoad, setLoading] = useState(true);
     const { addPermission, response } = useService();
+    const [responseTypes, setResponse] = useState([]);
+    const { loading, callEndpoint } = useFetchAndLoad();
 
     const savePermission = () => {
         setLoading(true);
@@ -26,23 +27,22 @@ const CreatePermissionPage = () => {
     }
 
     const GetPermissionTypes = () => {
-        const { loading, callEndpoint } = useFetchAndLoad();
-        const [response, setResponse] = useState([]);
         const getApiData = async () => await callEndpoint(getPermissionTypes());
-
         const adaptResponse = (data: []) => {
+            setLoading(false);
             setResponse(data);
         }
         useAsync(getApiData, adaptResponse, () => { });
-        return (
-            <>
-                {loading ? 'LOADING' :
-                   response.map((permissionType:PermissionType,key)=> {
-                       return <option key={key} value={permissionType.permissionTypeId}>{permissionType.description}</option>
-                   })
-                }
-            </>
-        );
+        return (<><option>No hay datos en la lista</option></>);
+    }
+
+    const ListPermissionTypes = () => {
+        return (<>{
+            responseTypes.map((permissionType: PermissionType, key) => {
+                return <option key={key} value={permissionType.permissionTypeId}>{permissionType.description}</option>
+            })}
+        </>
+        )
     }
 
     return (
@@ -54,12 +54,12 @@ const CreatePermissionPage = () => {
                 <Form.Control type="text" placeholder="Password" className="form-control" value={employyeLastName} onChange={e => setLastName(e.target.value)} />
             </FloatingLabel>
             <div className="div-button">
-                <Form.Select aria-label="Default select example" onChange={e=>setPermissionType(Number.parseInt(e.target.value))}>
-                    {<GetPermissionTypes/>}
+                <Form.Select aria-label="Default select example" value={permissionTypeSelect} onChange={e => setPermissionType(Number.parseInt(e.target.value))}>
+                    {responseTypes.length > 0 ? <ListPermissionTypes /> : <GetPermissionTypes />}
                 </Form.Select>
             </div>
             <div className="d-grid gap-2 div-button">
-                <Button variant="primary" type="submit" size="sm" disabled={loading} onClick={savePermission}>
+                <Button variant="primary" type="submit" size="sm" disabled={initLoad} onClick={savePermission}>
                     Submit
                 </Button>
                 {response && <h1>Guardado</h1>}
